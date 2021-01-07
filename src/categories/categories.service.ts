a20eff24ee465bf2c4891a862f2bed82132fea94
kadonomaro <denis.afer@gmail.com>
2021-01-08 00:49:21 +0700

    feat: add categories service error handling

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -11,35 +11,60 @@ export class CategoriesService {
   ) {}
 
   async getAll(): Promise<Category[]> {
-    return await this.categoriesRepository.find();
+    try {
+      return await this.categoriesRepository.find();
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   async getById(_id: string): Promise<Category> {
-    return await this.categoriesRepository.findOne({ where: [{ id: _id }] });
+    try {
+      return await this.categoriesRepository.findOne({
+        where: [{ id: _id }],
+      });
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   async create(category: Category): Promise<Category> {
-    return await this.categoriesRepository.save(category);
+    try {
+      return await this.categoriesRepository.save(category);
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   async update(_id: string, category: Category): Promise<Category> {
-    const updatedCategory = await this.categoriesRepository.findOne(_id);
-    if (!updatedCategory) {
-      throw new NotFoundException('Category is not found');
+    try {
+      const updatedCategory = await this.categoriesRepository.findOne(_id);
+      if (!updatedCategory) {
+        throw new NotFoundException('Category is not found');
+      }
+      updatedCategory.name = category.name;
+      updatedCategory.display_name = category.display_name;
+      updatedCategory.description = category.description;
+      updatedCategory.preview_image = category.preview_image;
+      updatedCategory.detail_image = category.detail_image;
+      updatedCategory.seo_title = category.seo_title;
+      updatedCategory.seo_description = category.seo_description;
+      updatedCategory.seo_slug = category.seo_slug;
+      return await updatedCategory.save();
+    } catch (error) {
+      console.error(error);
     }
-    updatedCategory.name = category.name;
-    updatedCategory.display_name = category.display_name;
-    updatedCategory.description = category.description;
-    updatedCategory.preview_image = category.preview_image;
-    updatedCategory.detail_image = category.detail_image;
-    updatedCategory.seo_title = category.seo_title;
-    updatedCategory.seo_description = category.seo_description;
-    updatedCategory.seo_slug = category.seo_slug;
-    return await updatedCategory.save();
   }
 
   async remove(_id: string): Promise<Category> {
-    const removedProduct = await this.categoriesRepository.findOne(_id);
-    return await this.categoriesRepository.remove(removedProduct);
+    try {
+      const removedCategory = await this.categoriesRepository.findOne(_id);
+      if (!removedCategory) {
+        throw new NotFoundException('Category is not found');
+      }
+      return await this.categoriesRepository.remove(removedCategory);
+    } catch (error) {
+      console.error(error);
+    }
   }
 }
