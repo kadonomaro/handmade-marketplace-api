1c073adf8471ccc95b23494f5eeb07c4c301d69e
kadonomaro <denis.afer@gmail.com>
2021-01-10 14:24:52 +0700

    doc: add open API swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 const PORT = 8000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  const options = new DocumentBuilder()
+    .setTitle('Handmade-marketplace API')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/docs', app, document);
+
   await app.listen(PORT, () => {
     console.log(`Server has been started on ${PORT} port`);
   });
